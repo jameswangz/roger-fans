67440e275da86c070d4fe48507386ad46f225680
James Wang <java2enterprise@gmail.com>
2013-08-25 00:11:24 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RogerFans::Application.config.secret_key_base = 'a55f9841c978f86fd330f089945a28d7edb0968e886df20060c9d62f3933b7a8dd247470c9efd600889d795d531b061529bf3f10148bfddb3573c3bf8e9d966b'
